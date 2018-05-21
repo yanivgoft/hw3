2d9fbe7ec7bd0e7b97b13a2c32868039cd8aa071
asafazar92 <39277840+asafazar92@users.noreply.github.com>
2018-05-21 20:18:17 +0300

    Add files via upload

--- listFilter.c
+++ listFilter.c
@@ -3,50 +3,41 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-struct node_t {
-    int n;
-    Node next;
-} node_t*;
-typedef bool (*CmpFunction) (int a,int b);
-typedef struct node_t* Node;
 
-Node listFilter(Node cur_node,CmpFunction boolCond,int divider);
-void nodeCopy(Node source_node,dest_node);
-
-
-
-Node listFilter(Node cur_node,CmpFunction boolCond,int divider) {
-    if (node == NULL || boolCond == NULL) {
+Node listFilter(Node list,CmpFunction boolCond,int divider) {
+    if (list == NULL || boolCond == NULL) {
         return NULL;
     }
 
-    while (!boolcond(cur_node->n,divider)) { //find first "good" node
-        cur_node = cur_node->next;
-        if(cur_node==NULL){
-            return NULL;
-        }
+    while (!boolCond(divider,list->n)) { //find first "good" node
+        list = list->next;
     }
-    Node starting_node = malloc(sizeof((Node)));
+    if(!list->next && !boolCond(divider,list->n){//only one "good" node
+        return NULL;
+    }
+    if(!list->next){
+        return list;
+    }
+    Node starting_node = nodeCreate(list->n);
     if (starting_node == NULL) {
         return NULL;;
     }
-    nodeCopy(cur_node,starting_node);
-    cur_node=cur_node->next;
-    starting_node->next=cur_node;
+    list=list->next;
+    starting_node->next=list;
     Node updated_node=starting_node;
-    while (!(cur_node->next)) {
-        if (!(boolCond(cur_node->n,divider))) {
-            cur_node=cur_node->next;
+    while (!(list->next)) {
+        if (!(boolCond(divider,list->n))) {
+            list=list->next;
 
         }
         else {
-            Node temp_node=malloc(sizeof(Node));
+            Node temp_node=nodeCreate(list->n));
             if(temp_node==NULL) {
-                destroyList(starting_node);
+               // destroyList(starting_node);
+                return NULL;
             }
-            nodeCopy(cur_node,temp);
-            updated_node=temp;
-            cur_node->next=cur_node;
+            updated_node->next=temp_node;
+            list=list->next;
 
         }
 
@@ -55,7 +46,12 @@ Node listFilter(Node cur_node,CmpFunction boolCond,int divider) {
 
 }
 
-void nodeCopy(Node source_node,Node dest_node){
-    dest_node->n=source_node->n;
-    dest_node->next=source_node->next;
+Node nodeCreate(int n){
+    Node new_node=malloc(sizeof(new_node));
+    if(!new_node){
+        return NULL;
+    }
+    new_node->n=n;
+    new_node->next=NULL;
+    return new_node;
 }
